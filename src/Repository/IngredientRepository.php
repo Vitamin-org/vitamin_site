3ca46fd3b66c3ea03f4c47fffb2d65be5060957c
atheostheos <coal.marcos@gmail.com>
2021-06-03 21:20:52 +0300

    AVS-24 Unnecessary files deleted

--- src/Repository/IngredientRepository.php
+++ src/Repository/IngredientRepository.php
@@ -40,36 +40,4 @@ class IngredientRepository extends ServiceEntityRepository
 
         return $actualVitamins;
     }
-
-
-
-
-    // /**
-    //  * @return Ingredient[] Returns an array of Ingredient objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('i')
-            ->andWhere('i.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('i.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Ingredient
-    {
-        return $this->createQueryBuilder('i')
-            ->andWhere('i.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
--- src/Repository/TemplateRepository.php
+++ src/Repository/TemplateRepository.php
@@ -31,33 +31,4 @@ class TemplateRepository extends ServiceEntityRepository
 
         return $this->find($idS[0]);
     }
-
-    // /**
-    //  * @return Template[] Returns an array of Template objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('t.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Template
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
